1a905f9ae7d783c90ff9fc0f621816fa79ac88e6
Vandna Patidar <vandna@monocept.com>
2023-06-23 12:43:18 +0530

    day9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/contact.dart';
 import 'package:flutter_catalog/widgets/profile.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -18,10 +19,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: MyThemes.lightTheme(context),
+      darkTheme: MyThemes.darkTheme(context),
       debugShowCheckedModeBanner: false,
       // darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: MyRoute.homeRoute,
